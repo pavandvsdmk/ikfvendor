19fd6b8675f8a6feecad3fc8c5628f7c8073c387
yogeshds10 <yogeshds10@gmail.com>
2018-08-31 15:21:58 +0530

    vendors list

--- src/app/vendor-list/vendor-list.component.ts
+++ src/app/vendor-list/vendor-list.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorListComponent implements OnInit {
 
+  vendors: Array<Object> = [];
+
+  settings = {
+    pager: {
+      display: true,
+      perPage: 10
+    },
+    actions: {
+      add: false,
+      delete: false,
+      edit: false
+    },
+    columns: {
+      firm: {
+        title: 'Firm',
+        width: '150px'
+      },
+      propreitor: {
+        title: 'propreitor'
+      },
+      Mobile: {
+        title: 'Mobile'
+      },
+      email: {
+        title: 'email'
+      }
+    },
+  };
+
   constructor() { }
 
   ngOnInit() {
